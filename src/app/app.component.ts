8dea9b2421fa28327c14a5015945d49469478bc0
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-05-09 18:10:17 -0300

    Iniciando estudos de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListComponentComponent } from './components/list-component/list-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponentComponent, ListComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-app';
